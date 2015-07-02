b359cf4c45af5a0ab89d7dbde1d56bb6bdd5f8aa
ycalero <mycalero@gmail.com>
2015-07-02 12:50:18 +0200

    random version1

---
+++ random.js
@@ -0,0 +1,8 @@
+// Math.random() devuelve un numero aleatorio
+var numero = Math.random();
+if (numero <= 0.5) {
+  console.log ('\n' + numero + 'Menor que 0.5');
+}
+else {
+  console.log('\n' + numero + 'Mayor que 0.5');
+}
